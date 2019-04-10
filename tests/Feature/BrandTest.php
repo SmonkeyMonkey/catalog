32daefdd336472eba7df0a25bd8838f5b66d37bf
SmonkeyMonkey <maaaaax9@gmail.com>
2019-04-10 16:55:16 +0300

    added based brand test

--- tests/Feature/BrandTest.php
+++ tests/Feature/BrandTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Http\UploadedFile;
 use Tests\TestCase;
 use App\Brand;
 use App\User;
@@ -22,18 +23,32 @@ class BrandTest extends TestCase
 
         $response->assertStatus(200);
     }
+
     /** @test */
-    public function admin_create_new_brand(){
+    public function admin_create_new_brand()
+    {
         \Session::start();
-        $brand = factory(Brand::class)->create();
+        \Storage::fake('images');
         $admin = factory(User::class)->create();
-        $response = $this->actingAs($admin)->json('post','admin/brand',[
-            'title' => 'this is title ',
-            'about'  => 'here text text text text',
+        $response = $this->actingAs($admin)->json('post', 'admin/brand', [
+            'title' => 'this is title',
+            'about' => 'here text text text text',
             'description' => 'and description asddsa',
-            'brand_id' => 1,
+            'image' => UploadedFile::fake()->image('no-img.jpg'),
             '_token' => csrf_token()
         ]);
         $response->assertStatus(302);
+        tap(Brand::first(),function ($brand){
+           $this->assertEquals('this is title',$brand->title);
+           $this->assertEquals('here text text text text',$brand->about);
+           $this->assertEquals('and description asddsa',$brand->description);
+           \Storage::disk('images')->assertMissing('files/no-img.jpg');
+        });
+    }
+    /** @test */
+    public function admin_can_delete_brand(){
+        $this->withoutMiddleware();
+        $response = $this->delete('admin/brand/1');
+        $this->assertEquals(302,$response->getStatusCode());
     }
 }
